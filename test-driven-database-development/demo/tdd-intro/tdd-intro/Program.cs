829250e6ece742e2895e57e2057c639bc63867de
Magnus Ahlkvist <magnus@tsql.nu>
2021-08-30 08:59:49 +0200

    c-sharp project for tdd added

---
+++ test-driven-database-development/demo/tdd-intro/tdd-intro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace tdd_intro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm not really going to use Main today...");
+        }
+        public static bool IsValidDate(string inDate)
+        {
+            var r = new Regex("\\d\\d\\d\\d-\\d\\d-\\d\\d");
+            var m = r.Match(inDate);
+            return m.Success;
+        }
+    }
+}
---
+++ test-driven-database-development/demo/tdd-intro/tdd-introTests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tdd_intro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_intro.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void IsValidDateTest_PositiveTest_StringFormat()
+        {
+            Assert.IsTrue(tdd_intro.Program.IsValidDate("2021-08-30"));
+        }
+        [TestMethod()]
+        public void IsValidDateTest_NegativeTest_StringFormat()
+        {
+            Assert.IsFalse(tdd_intro.Program.IsValidDate("08/30/2021"));
+        }
+    }
+}
