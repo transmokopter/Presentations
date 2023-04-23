72c116ba0d373230e597275bd9daecacf2fcaff9
Magnus Ahlkvist <magnus@tsql.nu>
2023-04-23 13:25:32 +0200

    ..

--- sql-in-azure-containers/code/build-app-container.ps1
+++ sql-in-azure-containers/code/build-app-container.ps1
@@ -1,4 +1,4 @@
 docker build .\app -t stateprovinceapp:1.1 
-docker tag stateprovinceapp:1.0 transmokopterpasssummit.azurecr.io/app/stateprovinceapp:1.1
-docker login transmokopterpasssummit.azurecr.io -u "transmokopterpasssummit" -p "JtV33GSfPrQidweQ0=LHIy3sMWgGNplq"
+docker tag stateprovinceapp:1.1 transmokopterpasssummit.azurecr.io/app/stateprovinceapp:1.1
+docker login transmokopterpasssummit.azurecr.io -u "transmokopterpasssummit" -p "Pfv4jJh9E/ke/TVC+lD/iXhWQmhtO2t5rmS0ZmTZ04+ACRA5PrwT"
 docker push transmokopterpasssummit.azurecr.io/app/stateprovinceapp:1.1
--- sql-in-azure-containers/code/build-db-container.ps1
+++ sql-in-azure-containers/code/build-db-container.ps1
@@ -1,4 +1,4 @@
 docker build .\db -t adventureworks2014-db:2.0 --build-arg DBNAME=Adventureworks2014 --build-arg PASSWORD="Pa55w.rd" --build-arg DACPAC="adventureworks2014-db/adventureworks2014-db/bin/Debug/adventureworks2014-db.dacpac" 
 docker tag adventureworks2014-db:2.0 transmokopterpasssummit.azurecr.io/db/aw2014:1.0
-docker login transmokopterpasssummit.azurecr.io -u "transmokopterpasssummit" -p "JtV33GSfPrQidweQ0=LHIy3sMWgGNplq"
+docker login transmokopterpasssummit.azurecr.io -u "transmokopterpasssummit" -p "Pfv4jJh9E/ke/TVC+lD/iXhWQmhtO2t5rmS0ZmTZ04+ACRA5PrwT"
 docker push transmokopterpasssummit.azurecr.io/db/aw2014:1.0
