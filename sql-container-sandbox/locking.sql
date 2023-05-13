5adaa1b64f37aa4450e307243713bf8ee03d3ad4
transmokopter <magnus@tsql.nu>
2023-05-13 09:20:28 +0200

    some stupid workloads

--- sql-container-sandbox/locking.sql
+++ sql-container-sandbox/locking.sql
@@ -1,2 +1,2 @@
-SELECT * FROM sys.dm_os_waiting_tasks AS DOWT WHERE session_id IN (85,87) AND wait_type<>'WAITFOR';
+SELECT * FROM sys.dm_os_waiting_tasks AS DOWT WHERE session_id IN (67,97) AND wait_type<>'WAITFOR';
 
