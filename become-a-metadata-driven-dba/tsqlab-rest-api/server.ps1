4e3b9577f69091604388184f0a164f9f63177a00
unknown <tsqlabadmin@TRANSMOKOPTER.LOCAL>
2024-06-13 00:29:53 +0000

    changed pode server config

--- become-a-metadata-driven-dba/tsqlab-rest-api/server.ps1
+++ become-a-metadata-driven-dba/tsqlab-rest-api/server.ps1
@@ -1,5 +1,6 @@
 Start-PodeServer {
     Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http 
+    Add-PodeEndpoint -Address 10.1.0.5 -Port 8080 -Protocol Http
 
     Add-PodeRoute -Method Get -Path 'GetSchedule' -ScriptBlock {
         $result = Get-Content "$psscriptroot\..\metadata\schedules.json" | ConvertFrom-Json
@@ -63,37 +64,6 @@ Start-PodeServer {
         (New-PodeOAStringProperty -Name 'SqlVersion' | ConvertTo-PodeOAParameter -In Query),
         (New-PodeOAStringProperty -Name 'Tier' | ConvertTo-PodeOAParameter -In Query),
         (New-PodeOAStringProperty -Name 'ConnectionName' | ConvertTo-PodeOAParameter -In Query)
-    )
-
-    Add-PodeRoute -Method Get -Path '/StartHelloJob' -ScriptBlock {
-        $job = Start-Job -Name TSQLABHelloJob -ScriptBlock {
-            Start-Sleep -Seconds 60
-        }
-        $jobnumber = $job.Id
-        $result = "Job $jobnumber started" | ConvertTo-Json        
-        Write-PodeJsonResponse -Value $result 
-    } 
-    Add-PodeRoute -Method Get -Path '/GetStartedJobs' -ScriptBlock{
-        $result = Get-Job | Select-Object -Property PSBeginTime, PSEndTime, Name, Id, State
-        $result | Get-Member | ForEach-Object{
-            $property = $PSItem.Name
-            $propertyValue = $WebEvent.Query[$property]
-            if($null -ne $propertyValue -and "" -ne $propertyValue -and $property -ne "PSBeginTime"){
-                $result = $result | Where-Object { $PSItem.$property -eq $propertyValue }
-            }
-        }
-        if($null -ne $WebEvent.Query["PSBeginTime"] -and "" -ne $WebEvent.Query["PSBeginTime"]){
-            $beginTime = [datetime]::ParseExact(($WebEvent.QUery["PSBeginTime"]),"yyyyMMddHHmmss",$null)
-            $result = $result | Where-Object { $PSItem.PSBeginTime -ge $beginTime }
-        }
-        if($null -eq $result){
-            $result = "" | ConvertTo-Json
-        }
-        Write-PodeJsonResponse $result 
-    } -PassThru |     Set-PodeOARequest -Parameters @(
-        (New-PodeOAStringProperty -Name 'Name' | ConvertTo-PodeOAParameter -In Query),
-        (New-PodeOAStringProperty -Name 'State' | ConvertTo-PodeOAParameter -In Query),
-        (New-PodeOAStringProperty -Name 'PSBeginTime' | ConvertTo-PodeOAParameter -In Query)
     )
 
 
